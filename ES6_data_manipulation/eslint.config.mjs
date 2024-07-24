5912d2b28d80a4a07e77219a61b73ef703fe69f7
samiribrh <samiribrahimov2277@gmail.com>
2024-07-24 19:58:37 +0400

    getting student by location

--- ES6_data_manipulation/eslint.config.mjs
+++ ES6_data_manipulation/eslint.config.mjs
@@ -1,8 +1,8 @@
 export default [
   {
     rules: {
-      "no-unused-vars": "error",
-      "no-undef": "error"
-    }
-  }
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+    },
+  },
 ];
