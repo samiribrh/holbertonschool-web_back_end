583e51bd890c828550e23951d8cd485acd72d373
samiribrh <samiribrahimov2277@gmail.com>
2024-08-17 00:13:50 +0400

    precreated function annotation

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for function that finds element length."""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
