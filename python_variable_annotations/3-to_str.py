78411501ccb20f1bf898e779806cdbc9db509180
samiribrh <samiribrahimov2277@gmail.com>
2024-08-16 23:59:16 +0400

    string representation of a float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for function that returns string representation of float."""
+
+
+def to_str(n: float) -> str:
+    """Return string representation of float."""
+    return str(n)
