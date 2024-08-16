0cf231975ff46cbb3558e1c0c42ca2a3973a36b5
samiribrh <samiribrahimov2277@gmail.com>
2024-08-16 23:55:47 +0400

    concatenating function

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Mpdule for function that concatenates two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings."""
+    return str1 + str2
