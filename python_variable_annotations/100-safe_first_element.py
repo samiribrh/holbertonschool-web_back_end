9de39d712964bda7eff6636f022615060c787db4
samiribrh <samiribrahimov2277@gmail.com>
2024-08-17 14:56:07 +0400

    Optional and Any

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module containing safe first element function."""
+from typing import Any, Sequence, Optional
+
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    if lst:
+        return lst[0]
+    else:
+        return None
