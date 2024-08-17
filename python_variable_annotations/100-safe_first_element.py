1c07727a3c0e4993c18f6b72d564a3873e874503
samiribrh <samiribrahimov2277@gmail.com>
2024-08-17 14:56:53 +0400

    Documentation

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -4,6 +4,7 @@ from typing import Any, Sequence, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """Safe first element function."""
     if lst:
         return lst[0]
     else:
