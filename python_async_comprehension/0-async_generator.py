15c0257527b319bd5fe988580ca6d8648ec125b2
samiribrh <samiribrahimov2277@gmail.com>
2024-08-19 11:57:43 +0400

    better annotation, pycodestyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator:
+async def async_generator() -> Generator[float, None, None]:
     """Function for asynchronous generator."""
     for _ in range(10):
         await asyncio.sleep(1)
