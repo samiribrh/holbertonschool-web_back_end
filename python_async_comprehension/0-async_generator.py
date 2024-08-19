bbff5b0a3b0b6c52c7930ee8b12bcf135f43bcd5
samiribrh <samiribrahimov2277@gmail.com>
2024-08-19 11:46:11 +0400

    Async generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module for asynchronous generator."""
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """Function for asynchronous generator."""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
