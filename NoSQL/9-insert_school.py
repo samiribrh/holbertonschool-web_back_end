2e288d3aa1e1cfe27ebda3d3e5561879b199db6d
samiribrh <samiribrahimov2277@gmail.com>
2024-08-19 14:06:30 +0400

    insert_one

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -6,4 +6,4 @@ def insert_school(mongo_collection, **kwargs):
     """Function for inserting new schools"""
     if len(kwargs) == 0:
         return None
-    return mongo_collection.insert(kwargs)
+    return mongo_collection.insert_one(kwargs)
