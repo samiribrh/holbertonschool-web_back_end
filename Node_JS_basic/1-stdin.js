8def99fbf5c1307aac6d98ebd3a6a9c38d904b10
samiribrh <samiribrahimov2277@gmail.com>
2024-09-19 15:21:44 +0400

    add checks:

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,12 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.stdout.write("This important software is now closing\n");
-  process.exit();
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.resume();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+  if (process.stdin.isTTY) {
+    process.exit();
+  } else {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
 });
