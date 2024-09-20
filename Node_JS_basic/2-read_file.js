e2d13ac066d55691755f9ec6b5d8cf49010dc192
samiribrh <samiribrahimov2277@gmail.com>
2024-09-20 08:57:30 +0400

    change

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,26 +1,36 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  let fileData;
-
   try {
-    fileData = fs.readFileSync(path, 'utf8');
+    const lines = fs.readFileSync(path, { encoding: 'utf8' }).split(/\r?\n/);
+    let countStudents = 0;
+    const fields = {};
+
+    for (let i = 1; i < lines.length; i += 1) {
+      const line = lines[i].trim();
+      if (line !== '') {
+        countStudents += 1;
+        const [fname, , , field] = line.split(',');
+        if (!fields[field]) {
+          fields[field] = {
+            countField: 1,
+            students: [fname],
+          };
+        } else {
+          fields[field].countField += 1;
+          fields[field].students.push(fname);
+        }
+      }
+    }
+    console.log(`Number of students: ${countStudents}`);
+    for (const field of Object.keys(fields)) {
+      const n = fields[field].countField;
+      const names = fields[field].students.join(', ');
+      console.log(`Number of students in ${field}: ${n}. List: ${names}`);
+    }
   } catch (err) {
     throw new Error('Cannot load the database');
   }
-
-  console.log(`Number of students: ${fileData.split('\n').length - 2}`);
-
-  const CS = fileData.split('\n').filter((line) => line.includes('CS')).length;
-  const SWE = fileData.split('\n').filter((line) => line.includes('SWE')).length;
-  let CSstudents = fileData.split('\n').filter((line) => line.includes('CS')).map((line) => line.split(',')[0]);
-  let SWEstudents = fileData.split('\n').filter((line) => line.includes('SWE')).map((line) => line.split(',')[0]);
-
-  CSstudents = CSstudents.join(', ');
-  SWEstudents = SWEstudents.join(', ');
-
-  console.log(`Number of students in CS: ${CS}. List: ${CSstudents}`);
-  console.log(`Number of students in SWE: ${SWE}. List: ${SWEstudents}`);
 }
 
 module.exports = countStudents;
